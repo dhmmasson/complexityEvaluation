681ab24b74e498d4bb4cfb6697e12ace0bff8518
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2025-05-06 11:07:27 +0200

    feat(api): serve static files

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
+import { stat } from "node:fs";
 import { Application, Router, Context } from "jsr:@oak/oak";
 import { join } from "jsr:@std/path";
 
 const OPENAPI_PATH = "./api.yml";
 
-const router = new Router();
+//Statically serve what is in _site
+
+//Server from api/v1
+const router = new Router({
+  prefix: "/api/v1",
+});
 
 // POST /user/
 router.post("/user", async (ctx: Context) => {
@@ -57,16 +63,72 @@ router.post("/quizz/:userId", async (ctx: Context) => {
 router.get("/openapi.yaml", async (ctx: Context) => {
   ctx.response.status = 200;
   ctx.response.type = "application/x-yaml";
-  ctx.response.headers.set(
-    "Content-Disposition",
-    "attachment; filename=api.yml"
-  );
+  // ctx.response.headers.set(
+  //   "Content-Disposition",
+  //   "attachment; filename=api.yml"
+  // );
   ctx.response.body = await Deno.open(OPENAPI_PATH);
 });
 
+// // Create a new router to serve the static html, css... (directly from root to _site)
+const staticRouter = new Router();
+
+const mimeTypes = {
+  html: "text/html",
+  css: "text/css",
+  js: "application/javascript",
+  json: "application/json",
+  png: "image/png",
+  jpg: "image/jpeg",
+};
+
+staticRouter.use(async (ctx: Context) => {
+  console.log("Static file request:", ctx.request.url.pathname);
+  const filePath = ctx.request.url.pathname;
+  const file = await Deno.open(join("_site", filePath), {
+    read: true,
+    write: false,
+    create: false,
+  });
+
+  const extension = filePath.split(".").pop();
+  ctx.response.type = mimeTypes[extension] || "application/octet-stream";
+  if (ctx.response.type === "application/octet-stream") {
+    console.warn("Unknown file type for", filePath);
+  }
+  ctx.response.body = file;
+});
+
 const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
+
+app.use(router.routes(), router.allowedMethods(), async (context, next) => {
+  try {
+    // Check if the request is for a static file
+    let filePath = context.request.url.pathname;
+
+    // check if / replace with index.html
+    if (filePath === "/") {
+      filePath = "/index.html";
+    }
+    if (!filePath.startsWith("/api/v1/")) {
+      const file = await Deno.open(join("_site", filePath), {
+        read: true,
+        write: false,
+        create: false,
+      });
+      const extension = filePath.split(".").pop();
+      context.response.type =
+        mimeTypes[extension] || "application/octet-stream";
+      if (context.response.type === "application/octet-stream") {
+        console.warn("Unknown file type for", filePath);
+      }
+      context.response.body = file;
+      return;
+    }
+  } catch {
+    await next();
+  }
+});
 
 console.log("Server running on http://localhost:8000");
 await app.listen({ port: 8000 });
